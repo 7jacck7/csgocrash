8004ac9b2d95490c72c487018ef86ab499d5babc
FreeSapling <uber.owner@hotmail.com>
2016-05-13 01:27:08 +0200

    Update AutoImproved.js

--- AutoImproved.js
+++ AutoImproved.js
@@ -1,11 +1,15 @@
-// Version 1.1 - Created by FREEDOM
+// Version 1.2 - Created by FREEDOM
 // This is exactly the same as the AUTO function on the site except this one throws in
 // a random amount of rounds to stop betting when it lost only to begin again after
-// waiting. This is to reduce the chance of riding a red train while afking.
+// waiting. This is to reduce the chance of it riding a red train while afking.
 
 var betAmount = 2000;
 var cashOut = 1.5; // x
 var noNegative = -10000; // If your net goes this much negative it stops betting
+var Rmin = 5; // Minimum amount of rounds to wait before betting again after a loss
+var Rmax = 10; // Maximum amount of rounds to wait before betting again after a loss
+
+
 //============= dont modify past this line ==================
 var net = 0;
 var minBet = 250;
@@ -27,15 +31,16 @@ var count = 0;
 engine.on('game_starting', function(info) {
     //console.log('Game Starting in ' + info.time_till_start);
 	if (pullout==0&&checkBet(betAmount)&&(net/100)>noNegative) {
-		console.log('Placing a bet: ',betAmount);
 		cashedOut = false;
 		engine.placeBet( betAmount*100, cashOut*100, true );
+		console.log('Placing a bet: ',betAmount);
 	}
 	else {
 		if (pullout>0) {
 		}
 		else {	
-		console.log('Bet amount invalid');
+		console.log('Bet amount invalid. Bot stopped');
+		engine.stop();
 		}
 	}
 });
@@ -52,12 +57,14 @@ engine.on('game_crash', function(data) {
 	if (gameResult=="WON"&&gameInside==true) {
 		gamesWon = gamesWon+1;
 		gamesPlayed = gamesPlayed+1;
+		logg();
 	}
 	else if (gameResult=="LOST"&&gameInside==true) {
 		gamesLost = gamesLost+1;
 		gamesPlayed = gamesPlayed+1;
-		pullout = Math.floor(Math.random() * 5)+5;
+		pullout = Math.floor(Math.random() * (Rmax-Rmin)) + Rmin;
 		console.log('Lost a bet, waiting for ',pullout,' rounds before betting again');
+		logg();
 	}
 	else if (gameInside==false&&pullout>0) {
 		pullout = pullout-1;
@@ -70,15 +77,7 @@ engine.on('game_crash', function(data) {
 	else if ( curBR > maxBR ) {
 		maxBR = curBR;                        
     }
-	net = curBR - startBR;   
-	if (count==10) {
-		logg();
-		count = 0;
-	}
-	else {
-		count++;
-	}
-	console.log('Net: ', net/100);
+	net = curBR - startBR;   	
 });
 
 engine.on('player_bet', function(data) {
@@ -105,8 +104,10 @@ engine.on('disconnect', function() {
     //console.log('Client disconnected');
 });
 function logg() {
+	console.log('\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n');
 	console.log('Total games played: ',gamesPlayed);
 	console.log('Total games won/lost: ',gamesWon,'/',gamesLost);
+	console.log('Net: ', net/100);
 }
 function checkBet(moni) {
 	return (moni>=250&&moni<=50000);
