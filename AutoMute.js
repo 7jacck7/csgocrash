217100600dc6b263967a82914466eddf6358620e
FreeSapling <uber.owner@hotmail.com>
2016-05-14 22:39:01 +0200

    Add files via upload

---
+++ AutoMute.js
@@ -0,0 +1,33 @@
+var nickname;
+var message;
+var id;
+var idS;
+var returnMessage;
+var bool = false;
+var 
+console.log('\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n');
+engine.on('msg', function(data) {
+    nickname = data.nickname;
+	message = data.message;
+	id = data.steamid;	
+	bool = false;
+	idS = id.toString();
+	for (var i = 0; i < message.length+1-idS.length; i++) {
+		if (message.substring(i,(i+idS.length))==idS) {
+			bool = true;
+		}
+	}
+	if (bool) {
+		else {
+			returnMessage = "Steam64 detected - "+nickname+", do not beg for coins in chat";
+			engine.chat(returnMessage);
+			returnMessage = "/mute "+idS+" 3h";
+			engine.chat(returnMessage);
+			console.log(nickname);			
+		}
+
+	}
+});
+
+
+
