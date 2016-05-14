d89d81e678a254e551f13c98ac6514665728f772
FreeSapling <uber.owner@hotmail.com>
2016-05-14 22:43:53 +0200

    Update AutoMute.js

--- AutoMute.js
+++ AutoMute.js
@@ -4,7 +4,7 @@ var id;
 var idS;
 var returnMessage;
 var bool = false;
-var 
+
 console.log('\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n');
 engine.on('msg', function(data) {
     nickname = data.nickname;
@@ -18,16 +18,13 @@ engine.on('msg', function(data) {
 		}
 	}
 	if (bool) {
-		else {
 			returnMessage = "Steam64 detected - "+nickname+", do not beg for coins in chat";
 			engine.chat(returnMessage);
-			returnMessage = "/mute "+idS+" 3h";
+			//returnMessage = "/mute "+idS+" 3h";
 			engine.chat(returnMessage);
 			console.log(nickname);			
-		}
+		
 
 	}
 });
-
-
 
